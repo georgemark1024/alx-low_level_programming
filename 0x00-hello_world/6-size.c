a4e940965444656b698f96b8f0b9f83dee2cd07a
George <makokumz.2020@gmail.com>
2023-10-05 15:54:21 -0700

    Size calc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d" sizeof(char) "bytes(s)")
-	printf("Size of an int: %d" sizeof(int) "bytes(s)")
-	printf("Size of a long int: %d"(long int)sizeof(int) "bytes(s)")
-	printf("Size of a long long: %d" (long long int)sizeof(int) "bytes(s)")
-	printf("Size of a float: %d" sizeof(float) "bytes(s)")
+	printf("Size of a char: %d" (char)sizeof(char) "bytes(s)");
+	printf("Size of an int: %d" (int)sizeof(int) "bytes(s)");
+	printf("Size of a long int: %d"(long int)sizeof(int) "bytes(s)");
+	printf("Size of a long long: %d" (long long int)sizeof(int) "bytes(s)");
+	printf("Size of a float: %d" (float)sizeof(float) "bytes(s)");
+	return (0);
 }
