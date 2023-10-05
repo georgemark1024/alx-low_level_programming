781d2900d564036f42b0a8e22db5c7e43f2caa36
George <makokumz.2020@gmail.com>
2023-10-05 15:10:29 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-int main(void) /* A function that prints a text on the std output */
+/** A function that prints a text on the std output
+ * It uses puts instead of printf
+ */
+int main(void)
 {
-	char *str1 = "Programming is like building a multilingual puzzle";
+	char *str1 = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 
