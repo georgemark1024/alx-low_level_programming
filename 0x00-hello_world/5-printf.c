a1abd8755362477c21042054611364f5c37f035a
George <makokumz.2020@gmail.com>
2023-10-05 15:28:59 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
-/* A function that prints using the printf function */
+/**
+ *  A function that prints using the printf function 
+ *  Description: prints using printf
+ *
+ *  Return: returns 0 to exit*/
 int main(void)
 {
 	char str[] = "with proper grammar, but the outcome is a piece of art,";
