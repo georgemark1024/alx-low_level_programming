c43ef596356f168195a23310c7fa8036d623c23a
George <makokumz.2020@gmail.com>
2023-10-05 17:06:12 -0700

    Data type size calculator

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
-/*
- * main - prints the size of various types depending on the computer
+/**
+ * main - Size calculator
  *
  * Description: It calculates the size in bytes on different data types
  *
