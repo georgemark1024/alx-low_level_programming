2909f556c5cc74080ed5dc5688bbca16336978c4
George <makokumz.2020@gmail.com>
2023-10-08 03:10:57 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 0; i < 16; i++)
 	{
-		if(i < 10)
+		if (i < 10)
 			putchar(i + '0');
 		else
 			putchar(i - 10 + 'a');
-
-		putchar('\n');
 	}
-	return(0);
+	putchar('\n');
+
+	return (0);
 }
