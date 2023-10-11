e7433efa3aa14427474a5df18f2f37cc36f4170a
George <makokumz.2020@gmail.com>
2023-10-11 14:37:48 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
-include "main.h"
+#include "main.h"
 
 int _putchar(char c);
 
-char x = 'a'
-int i = 0
+char x = 'a';
+int i = 0;
 /**
  * 10times - prints the alphabet in lowercase 10 times
  * Documentation:
