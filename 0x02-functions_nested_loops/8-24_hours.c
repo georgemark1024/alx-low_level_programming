9a552c2aac399163254c8ec48834c0c2c6c104ec
George <makokumz.2020@gmail.com>
2023-10-12 16:34:41 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,24 @@
-#include"main.h"
+#include "main.h"
 
 void jack_bauer(void)
 {
-	int w, x, y, z;
+    int hour_tens, hour_ones, minute_tens, minute_ones;
 
-	for (w = 0; w <= 2; w++)
-	{
-		for (x=0; x <= 9; x++)
-		{
-			if ((w <= 1 && w <= 9) || (w <= 2 && w <= 3))
-			{
-				for (y = 0; y <= 5; y++)
-				{
-					_putchar(w + '0');
-					_putchar(x + '0');
-					_putchar(58);
-					_putchar(y + '0');
-					_putchar(z + '0');
-					_putchar('\n');
-				}
-			}
-		}
-	}
+    for (hour_tens = 0; hour_tens <= 2; hour_tens++) {
+        for (hour_ones = 0; hour_ones <= 9; hour_ones++) {
+            if ((hour_tens < 2) || (hour_tens == 2 && hour_ones <= 3)) {
+                for (minute_tens = 0; minute_tens <= 5; minute_tens++) {
+                    for (minute_ones = 0; minute_ones <= 9; minute_ones++) {
+                        _putchar(hour_tens + '0');
+                        _putchar(hour_ones + '0');
+                        _putchar(':');
+                        _putchar(minute_tens + '0');
+                        _putchar(minute_ones + '0');
+                        _putchar('\n');
+                    }
+                }
+            }
+        }
+    }
 }
 
