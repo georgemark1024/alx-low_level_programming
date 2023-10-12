b93726be13a4f2f685f05846d3359fa5ed82ac14
George <makokumz.2020@gmail.com>
2023-10-12 14:11:32 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _abs - computes absolute value of an int
+ * @int: Int to be computed
+ * Return: 0 for success
+ */
+
+int _abs(int)
+{
+	int x;
+
+	if (int > 0)
+		_putchar('n');
+	else if (int == 0)
+		_putchar('0');
+	else
+	{
+		x = int * -1;
+		_putchar('x');
+	}
+	return (0);
+}
