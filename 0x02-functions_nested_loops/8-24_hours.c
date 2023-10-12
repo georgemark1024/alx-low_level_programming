04ebc7f2f007dc9d54c7b08acd5285e385303296
George <makokumz.2020@gmail.com>
2023-10-12 16:53:04 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,23 +2,27 @@
 
 void jack_bauer(void)
 {
-    int hour_tens, hour_ones, minute_tens, minute_ones;
+	int h_10, hour_ones, minute_10, m_1;
 
-    for (hour_tens = 0; hour_tens <= 2; hour_tens++) {
-        for (hour_ones = 0; hour_ones <= 9; hour_ones++) {
-            if ((hour_tens < 2) || (hour_tens == 2 && hour_ones <= 3)) {
-                for (minute_tens = 0; minute_tens <= 5; minute_tens++) {
-                    for (minute_ones = 0; minute_ones <= 9; minute_ones++) {
-                        _putchar(hour_tens + '0');
-                        _putchar(hour_ones + '0');
-                        _putchar(':');
-                        _putchar(minute_tens + '0');
-                        _putchar(minute_ones + '0');
-                        _putchar('\n');
-                    }
-                }
-            }
-        }
-    }
+	for (h_10 = 0; h_10 <= 2; h_10++)
+	{
+		for (hour_ones = 0; hour_ones <= 9; hour_ones++)
+		{
+			if ((h_10 < 2) || (h_10 == 2 && hour_ones <= 3))
+			{
+				for (minute_10 = 0; minute_10 <= 5; minute_10++)
+				{
+					for (m_1 = 0; m_1 <= 9; m_1++)
+					{
+						_putchar(h_10 + '0');
+						_putchar(hour_ones + '0');
+						_putchar(':');
+						_putchar(minute_10 + '0');
+						_putchar(m_1 + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
-
