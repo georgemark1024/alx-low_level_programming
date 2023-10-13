87cc9fe3cc0ecaba239861fcbffeacdfea8989d9
George <makokumz.2020@gmail.com>
2023-10-13 14:05:25 -0700

    Numberes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,16 @@
  * print_numbers - prints numbers from 0 to 9
  * Return: ...
  */
+
+int _putchar(char c);
+
 void print_numbers(void)
 {
 	char i = 0;
 
 	while (i <= 9)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
