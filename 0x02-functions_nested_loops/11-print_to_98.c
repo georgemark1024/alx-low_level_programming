e19808aed5ea7bb27747b2d9f46c2e174efdb668
George <makokumz.2020@gmail.com>
2023-10-12 17:25:19 -0700

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,18 @@
 #include"main.h"
 
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: starting point
+ * 
+ */
 void print_to_98(int n)
 {
+	while (n <= 98)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+		n++;
+	}
+}
 
