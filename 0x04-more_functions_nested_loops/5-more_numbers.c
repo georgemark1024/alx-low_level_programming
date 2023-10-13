1cb9e05c21b9a13ced66b4b9759907e01eb1e608
George <makokumz.2020@gmail.com>
2023-10-13 14:39:48 -0700

    Numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers form 0 to 14 10 times
+ * Return: ...
+ */
+
+void more_numbers(void)
+{
+	char i = 0;
+	int y = 0;
+
+	while (y <= 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			_putchar(i + '0');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
