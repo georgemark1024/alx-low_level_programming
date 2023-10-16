b91dafcdc577e64dd1ffacdee19c2f1632219798
George <makokumz.2020@gmail.com>
2023-10-16 14:38:26 -0700

    Size calc

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int_strlen - returns size of a string
+ * @s: the string
+ * Return: size
+ */
+
+int_strlen(char *s)
+{
+	int size;
+
+	size = sizeof (s) / 4;
+	return (size);
+}
