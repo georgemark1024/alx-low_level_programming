a1d71d342f76bef8ddf3c6fa7cf2f9911ddc28e0
George <makokumz.2020@gmail.com>
2023-10-16 15:13:18 -0700

    Char skipping 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints a char and skips the next one
+ * @str: String
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		continue;
+	}
+}
