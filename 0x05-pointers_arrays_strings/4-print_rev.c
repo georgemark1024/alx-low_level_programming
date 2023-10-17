461ee55054950c1f2ec8d09005e222eff4350e0d
George <makokumz.2020@gmail.com>
2023-10-17 00:41:36 -0700

    Reverse letters

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_rev - prints a string in reverse
+ * @s: string
+ * Return: void
  */
 
 void print_rev(char *s)
 {
-	while (
+	while (*s != '\0')
+		s++;
+		_putchar(*s);
+		while (*s <= '\0')
+		{
+			_putchar(*s);
+			s--;
+		}
+}
