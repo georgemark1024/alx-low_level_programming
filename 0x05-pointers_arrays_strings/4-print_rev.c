2b10a24a2c8d4f3309040d96e4d0428b420e7e17
George <makokumz.2020@gmail.com>
2023-10-17 11:29:06 -0700

    Reverse char

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,7 @@
  * @s: string
  * Return: void
  */
-
+/**
 void print_rev(char *s)
 {
 	while (*s != '\0')
@@ -20,4 +20,20 @@ void print_rev(char *s)
 			}
 		}
 	}
+}*/
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (int i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 }
+
