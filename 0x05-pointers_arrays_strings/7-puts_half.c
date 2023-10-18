8b73a6901cbebdcaad93447b0b149b1ed0f10a50
George <makokumz.2020@gmail.com>
2023-10-18 13:17:40 -0700

    Half str

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		n = (size - 1) / 2;
 		while (str[n] != '\0')
 		{
-			if (n = (size - 1) / 2)
+			if (n == (size - 1) / 2)
 				continue;
 			_putchar(str[n]);
 			n++;
