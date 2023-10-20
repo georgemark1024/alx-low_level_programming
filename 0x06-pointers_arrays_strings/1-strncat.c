18dd3cd63a5b3c948ff82513bc407171366b7899
George <makokumz.2020@gmail.com>
2023-10-20 02:14:52 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int d = 0, s = 0;
-
 	while (dest[d] != '\0')
 	{
 		d++;
 	}
 	while (s <= n)
 	{
+		if (src[s] == '\0')
+			break;
 		dest[d] = src[s];
 		d++;
 		s++;
