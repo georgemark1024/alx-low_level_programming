74c8a1d4839b44b942263821d0c14f1627424387
George <makokumz.2020@gmail.com>
2023-10-20 01:28:15 -0700

    str concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,43 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int tot_size, i = 0, j = 0;
-
-	tot_size = size(dest) + size(src) + 1;
-
-	char concat[tot_size];
+	int i = 0, j = 0;
 	
 	while (dest[i] != '\0')
 	{
-		concat[j] = dest[i];
+		i++;
+	}
+	while(src[j] != '\0')
+	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	i = 0;
-	while(src[i] != '\0')
-	{
-		concat[j] = src[i];
-		i++;
-		j++;
-	}
-	concat[tot_size] = '\0';
-	return (concat);
-}
-/**
- * tot_size - computes the size of a srings
- * @str_ptr: pointer to first char of a string str_ptr
- * Return: size
- */
-
-
-int size(char *str_ptr)
-{
-	int size = 0;
-
-	while (*str_ptr != '\0')
-	{
-		str_ptr++;
-		size++;
-	}
-	return (size);
+	dest[i] = '\0';
+	return (dest);
 }
