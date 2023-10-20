c537446630c0373cb2afd7891cc1481fa5692d30
George <makokumz.2020@gmail.com>
2023-10-20 01:31:35 -0700

    str concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,7 @@
  * _strcat - concatenates two strings
  * @dest: destination str
  * @src: source str
- * Return: pointer to des
- t
+ * Return: pointer to dest
  */
 
 char *_strcat(char *dest, char *src)
