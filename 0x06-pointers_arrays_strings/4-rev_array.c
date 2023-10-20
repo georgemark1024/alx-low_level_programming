77f595410b06c4fee6c7b65e7d422359d14bff56
George <makokumz.2020@gmail.com>
2023-10-20 13:23:59 -0700

    rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * reverse_array - reverses the content of an array
@@ -14,4 +15,15 @@ void reverse_array(int *a, int n)
 		_putchar(a[n]);
 		n--;
 	}
+}
+/** _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return:; On success 1
+ * On error. -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
