4c8c2a2b175475e8ec5aaf6606fdd6df473a6473
George <makokumz.2020@gmail.com>
2023-10-20 10:18:09 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings lexicographically
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if similar, -1 if s1 < s2, 1 if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int pos = 0;
+
+	while (s1[pos] == s2[pos])
+	{
+		pos++;
+		if (s1[pos] == '\0' && s2[pos] == '\0')
+			return (0);
+	}
+	if (s1[pos] < s2[pos])
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
