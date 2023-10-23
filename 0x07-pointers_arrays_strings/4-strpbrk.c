b164ea196f39a6997966b1bad8c034fe73dff5ec
George <makokumz.2020@gmail.com>
2023-10-23 14:04:07 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stddef.h>
+
+/**
+ * _strpbrk - locates first occurence of any chars in accept in s
+ * @s: string
+ * @accept: string for reference
+ * Return: pointer to the char in s which is in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (s[count] != '\0')
+	{
+		int i = 0;
+		while (accept[i] != '\0')
+		{
+			if (s[count] == accept[i])
+			{
+				return (s + count);
+			}
+			i++;
+		}
+		count++;
+	}
+	return (NULL);
+}
