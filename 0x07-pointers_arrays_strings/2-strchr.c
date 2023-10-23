0d2ac4b3550ad224190dc65601339528a0c57c6f
George <makokumz.2020@gmail.com>
2023-10-23 12:51:44 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a specific character in a string
+ * @s: string
+ * @c: character to be searched
+ * Return: pointer to the char if c is present in s, else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	for (; s[count] != '\0'; count++)
+	{
+		if (s[count] == c)
+		{
+			return (s + count);
+		}
+	}
+	return (NULL);
+}
