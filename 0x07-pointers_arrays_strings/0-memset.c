e593aa0aa78e0be12783ed40951aa0facdff0a85
George <makokumz.2020@gmail.com>
2023-10-23 11:42:24 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _memset - copies a constant to specified number of memory locations
- * @s: string 
+* _memset - copies a constant to specified number of memory locations
+ * @s: string
  * @b: constant character
  * @n: number of bytes of s where b will be copied
  * Return: modiified s
