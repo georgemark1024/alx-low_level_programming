b8ab955abfb5840267a2520075efc8dba8a60a43
George <makokumz.2020@gmail.com>
2023-10-23 11:47:42 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count = 0;
+
+	while (count <= n)
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	return (&dest);
+}
