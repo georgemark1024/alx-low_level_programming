519815a7b9209fca766c31a6221bd59a9c7e9be3
George <makokumz.2020@gmail.com>
2023-10-24 12:12:33 -0700

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_diagsums - prints the sum of numbers on diagonals of an array
+ * @a: interger array
+ * @size: row and column size of the array (square)
+ * Return: void
  */
 
 
 void print_diagsums(int *a, int size)
 {
 	int i = 0;
+	int sum;
 
-	while (i < size * size)
+	while (i < size)
 	{
 		int inner_count = 0;
 
 		while (inner_count < size)
 		{
 			 
-			sum = sum + a[i][inner_count + size + 1];
-			i = i + size + 1;
+			 sum = sum + a[i * size + inner_count];
+			inner_count++;
 		}
 	}
 }
