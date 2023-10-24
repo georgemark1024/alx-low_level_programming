4a8fd2964285994ae3d453d25bca1ec78f30aab8
George <makokumz.2020@gmail.com>
2023-10-24 10:15:21 -0700

    print_diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+
+	while (i < size * size)
+	{
+		int inner_count = 0;
+
+		while (inner_count < size)
+		{
+			 
+			sum = sum + a[i][inner_count + size + 1];
+			i = i + size + 1;
+		}
+	}
+}
