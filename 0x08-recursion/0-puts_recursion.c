acf4f429c0ad32778980f152d7533d222625a437
George <makokumz.2020@gmail.com>
2023-10-25 19:52:49 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,9 +10,10 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	if (s[i] == '\0')
 	{
-		_putchar(s[i]);
+		return (0);
 	}
+	_puts_recursion(s[i + 1]);
 	_putchar('\n');
 }
