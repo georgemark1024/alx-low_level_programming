e7274f1697e65ede247eb3e49435a6d0866da6fe
George <makokumz.2020@gmail.com>
2023-10-28 14:05:08 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,6 +9,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	*dest = *src;
-	return (&dest);
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
