2af023509bfdd5b99c09643414c5bda74a5b23b7
George <makokumz.2020@gmail.com>
2023-10-30 05:30:08 -0700

    prints_args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints all the arguments it receives
+ * @argc: number of elements in argv or the numebr of arguments
+ * passed to the program including the program name
+ * @agrv: array containing arguments passed to the command line
+ * Return: 0 on success, otherwise on failure
+ */
+
+int main(int argc, char **argv)
+{
+	int count = 0;
+
+	while (count < argc)
+	{
+		printf("%s\n", argv[count]);
+		count++;
+	}
+	return (0);
+}
+
