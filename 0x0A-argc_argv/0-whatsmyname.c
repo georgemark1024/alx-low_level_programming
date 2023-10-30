8c245aeb5edfcafe5c245c1b38063f5cae9f818a
George <makokumz.2020@gmail.com>
2023-10-30 04:30:16 -0700

    prog name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints the name of the program and a new line
+ * @agrc: length of array argv
+ * @argv: array containing arguments passed to the command line
+ * Return: 0 on zero success
+ */
+
+int main(int agrc, char **agrv)
+{
+	printf("%s\n", agrv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This dir contains work about the argc and argv concepts in C programming language
