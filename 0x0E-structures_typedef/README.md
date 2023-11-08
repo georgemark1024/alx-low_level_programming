011d9374194aad3d7617fc34b558d6e53e423076
George <makokumz.2020@gmail.com>
2023-11-08 04:35:23 -0800

    dogs struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains work done in C programming language about the concept of structures
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - contains info about dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
