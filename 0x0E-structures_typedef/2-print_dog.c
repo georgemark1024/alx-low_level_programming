d1f0e95da5be4ef79171f2d6b2f457f60edb75c8
George <makokumz.2020@gmail.com>
2023-11-08 05:49:52 -0800

    prints_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,7 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d.name, d.age, d.owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
 #endif
